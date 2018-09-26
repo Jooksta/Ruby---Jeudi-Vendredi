e817ae1c160d204f67dfbdf2107a0c500f844d39
Jooksta <teslachipset@gmail.com>
2018-09-26 09:21:47 +0200

    Demander à l'utilisateur_c

---
+++ exo_07_c.rb
@@ -0,0 +1,11 @@
+user_name = gets.chomp
+puts user_name
+
+
+#Les 3 programmes sont identiques, seul l'output est différent ;
+
+# Le programme exo_07_a.rb est identique sauf qu'il ne permet pas d'imprimer ce signe ">"
+
+# Le programme exo_07_b.rb permet d'imprimer le signe ">" afin d'indiquer à l'utilisateur qu'il doit écrire une réponse
+
+# Le programme exo_07_c.rb est identique sauf qu'il n'affiche pas de string pour poser une question
