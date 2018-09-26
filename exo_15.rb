a61f3ae89a8173403dde6b42f0d86a949819c25f
Jooksta <teslachipset@gmail.com>
2018-09-26 16:14:04 +0200

    Afficher tous les âges - modif code pour afficher l'age

--- exo_15.rb
+++ exo_15.rb
@@ -1,6 +1,10 @@
 puts "Quelle est ton année de naissance?"
 print ">"
 number = gets.to_i
-while number <= 2017
-    puts number += 1 
+Puts "Quelle est ton âge ?"
+print ">"
+number2 = gets.to_i
+
+while number < 2017
+    puts number += 1 number2 +=1
 end
