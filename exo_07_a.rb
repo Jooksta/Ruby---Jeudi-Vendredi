3d126492c8c1f39c5e0a2b76b75758975938b013
Jooksta <teslachipset@gmail.com>
2018-09-26 09:21:27 +0200

    Demander à l'utilisateur_a

---
+++ exo_07_a.rb
@@ -0,0 +1,12 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+
+# gets.chomp permet d'écrire une réponse dans le terminal.
+
+# gets demande à l'utilisateur d'écrire quelque chose.
+
+# chomp sert à retirer le caractère "\n" qui signifie un retour à la ligne.
+
+
