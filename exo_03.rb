72e45ccf517c6639544ba1e265a6e605c2b67bed
Jooksta <teslachipset@gmail.com>
2018-09-26 09:19:09 +0200

    il ne dit pas Bonjour

---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Bonjour Monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# En ajoutant # devant la ligne 2, la console n'affiche pas la phrase car elle est considérée comme un commentaire.
