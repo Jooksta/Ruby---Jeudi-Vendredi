01de36b69a48c56ded1c70faabdfc53095155270
Jooksta <teslachipset@gmail.com>
2018-09-26 19:01:59 +0200

    Annoncer Age option BG, Programme opérationnel

--- exo_16.rb
+++ exo_16.rb
@@ -1,7 +1,11 @@
 puts "Quel est ton âge?"
 print ">"
 age = gets.to_i
-year = 2018
+year = 2018 - age
+age = 0
 
-puts "en #{year} tu avais #{age} ans
-
+while year < 2018
+	puts "en #{year} tu avais #{age} ans !"
+	age += 1 
+	year += 1
+end
