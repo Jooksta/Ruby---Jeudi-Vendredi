208dcde17e7986afb0f3e66e579eb8b858adb19d
Jooksta <teslachipset@gmail.com>
2018-09-26 09:18:41 +0200

    Bonjours monde

---
+++ exo_02.rb
@@ -0,0 +1,5 @@
+puts "Bonjour Monde !"
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# print prend ce qu'on lui donne et l'imprime à l'écran sans retour à la ligne.
+# puts ajoute une nouvelle ligne (vide) après ce qu'on veut imprimer et permet ainsi un retour à la ligne.
