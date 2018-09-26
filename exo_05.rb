55cb07fb6583679a811f145c0ea14aa2195065ef
Jooksta <teslachipset@gmail.com>
2018-09-26 09:20:17 +0200

    opérations

---
+++ exo_05.rb
@@ -0,0 +1,43 @@
+
+# #{} sert à faire une interpolation, insérer des variables dans des strings sans avoir à se compliquer la vie avec les concatenations. 
+
+#imprime la string
+puts "On va compter le nombre d'heures de travail à THP" 
+
+#imprime la string et fait une multiplication de 3 chiffres
+puts "Travail : #{10 * 5 * 11}"
+
+#imprime la string et fait une multiplication de 4 chiffres
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#imprime la string
+puts "Et en secondes ?"
+
+#fait une multiplication de 5 chiffres
+puts 10 * 5 * 11 * 60 * 60
+
+#imprime la string 
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#vérifie si l'opération est true/false
+puts 3 + 2 < 5 - 7
+
+#imprime la string et fait une addition des 2 chiffres contenus dans les accolades
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#imprime la string et fait une soustraction des 2 chiffres contenus dans les accolades
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#imprime la string
+puts "Ok, c'est faux alors !"
+
+#imprime la string
+puts "C'est drôle ça, faisons-en plus :"
+
+#imprime la string et vérifie si l'opération dans l'accolade est true/false
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+
+#imprime la string et vérifie si l'opération dans l'accolade est true/false
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+
+#imprime la string et vérifie si l'opération dans l'accolade est true/false
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
