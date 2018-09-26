26d143e1c9a98f816b8aa0c4dcc4733e5fbf2a53
Jooksta <teslachipset@gmail.com>
2018-09-26 21:18:36 +0200

    Virer années, Fonctionnel !

--- exo_16.rb
+++ exo_16.rb
@@ -3,15 +3,12 @@ print ">"
 age = gets.to_i
 birth = 2018 - age
 year = 2018 - birth
+age = 0
 
-
-
-
-while age != 28
+while year >= 0
 	puts "il y a #{year} ans tu avais #{age} ans !"
 		
 		year -= 1
 		age += 1 
 	end
-	
 	
