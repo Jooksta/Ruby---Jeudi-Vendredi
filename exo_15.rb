6cc8146838ffc944e3f375a62d6e30dc03d8b1af
Jooksta <teslachipset@gmail.com>
2018-09-26 17:14:18 +0200

    Afficher tous les âges - Le programme fonctionne mais il faut appuyer 2 fois sur entrée quand on donne l'année de naissance

--- exo_15.rb
+++ exo_15.rb
@@ -1,16 +1,10 @@
 puts "Quelle est ton année de naissance?"
 print ">"
 number = gets.to_i
-age = "#{2018 - number}"
+age = 2018 - number
+age = gets.to_i
 
 while number < 2017
-    puts number += 1 , age
+    puts number += 1 
+    puts age += 1
 end
-
-    	
-
-
-
-
-
-
