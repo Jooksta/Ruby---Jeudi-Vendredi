0b7ae65782bd1d1f84395204342fddcd80844a5b
Jooksta <teslachipset@gmail.com>
2018-09-26 09:19:52 +0200

    marche pô

---
+++ exo_04.rb
@@ -0,0 +1,13 @@
+
+#La console retourne l'erreur suivante car il faut fermer les guillemets à la fin de la ligne ; 
+
+#unterminated string meets end of file
+
+#il faudrait donc l'écrire comme ceci ; 
+
+# puts "Salut, ça farte ?"
+
+puts "Salut, ça farte ?
+
+
+
