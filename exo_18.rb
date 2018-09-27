172a0123ddbdbf1360459fc06efd03fdbe7e8061
Jooksta <teslachipset@gmail.com>
2018-09-27 17:41:24 +0200

    Presque fonctionnel ?

--- exo_18.rb
+++ exo_18.rb
@@ -1,22 +1,27 @@
 list = []
+
 em = 00
 compteur = 00
-list << "jean.dupont.0"+"#{compteur}"+"@email.fr"
+
+
 
  	while em <= 50
 
  		if compteur < 9
- 			list << "jean.dupont.0"+"#{compteur}"+"@email.fr"
- 			compteur += 1
+ 			puts list << "jean.dupont.0"+"#{compteur}"+"@email.fr"
+ 			compteur = compteur + 1
 
- 			else
- 				list << "jean.dupont."+"#{compteur}"+"@email.fr"
- 				compteur += 1
+ 		else
+ 		puts list << "jean.dupont."+"#{compteur}"+"@email.fr"
+ 		compteur = compteur + 1
  			
     		
     	end
+
+	puts list
+	em = em + 1
+
     end    
 
        
-	puts list
-	em = em + 1
+	
