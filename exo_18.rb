7d55578c2fa91efc425b941c8eb286406f0d6728
Jooksta <teslachipset@gmail.com>
2018-09-27 17:11:25 +0200

    ne fonctionne pas - recherche du pb

--- exo_18.rb
+++ exo_18.rb
@@ -1,24 +1,22 @@
 list = []
 em = 00
-nb = 00
 compteur = 00
+list << "jean.dupont.0"+"#{compteur}"+"@email.fr"
 
- 	while (em <= 50)
+ 	while em <= 50
 
  		if compteur < 9
- 			list = ["jean.dupont.0" + compteur + "@email.fr"]
- 			compteur = compteur + 1
+ 			list << "jean.dupont.0"+"#{compteur}"+"@email.fr"
+ 			compteur += 1
 
  			else
- 				list = ["jean.dupont." + compteur + "@email.fr"]
- 				compteur = compteur + 1
+ 				list << "jean.dupont."+"#{compteur}"+"@email.fr"
+ 				compteur += 1
+ 			
     		
     	end
     end    
 
        
-puts list
-
-em = em + 1
-
-end
+	puts list
+	em = em + 1
