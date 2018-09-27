f643c83faeabda224adb3128c94ba513dea8f9e4
Jooksta <teslachipset@gmail.com>
2018-09-28 00:28:14 +0200

    Pyramide Illuminati - ajout condition sur l'input user

--- exo_20.rb
+++ exo_20.rb
@@ -2,6 +2,12 @@ puts "Choisi un nombre entre 1 et 25"
 puts ">"
 number=gets.to_i
 
+if number  < 1 || number > 25 
+	puts "Seulement entre 1 et 25"
+		exit
+end
+
+
 compteur = 1
 
 while compteur <= number
