89ab9e4f1e91fb047134e86386bb813e849fcdbc
Jooksta <teslachipset@gmail.com>
2018-09-27 16:06:30 +0200

    Modification du code entier

--- exo_18.rb
+++ exo_18.rb
@@ -1,9 +1,24 @@
+list = []
 em = 00
-nb= 01
- while (em <= 50)
- 	puts "jean.dupont.#{nb}@email.fr"
- 			nb +=1
+nb = 00
+compteur = 00
 
- 			em= em + 1
+ 	while (em <= 50)
+
+ 		if compteur < 9
+ 			list = ["jean.dupont.0" + compteur + "@email.fr"]
+ 			compteur = compteur + 1
+
+ 			else
+ 				list = ["jean.dupont." + compteur + "@email.fr"]
+ 				compteur = compteur + 1
+    		
+    	end
+    end    
+
+       
+puts list
+
+em = em + 1
 
 end
