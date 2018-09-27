7c5680565cbb44688cdf0a1fe10054acf9ca14b3
Jooksta <teslachipset@gmail.com>
2018-09-27 19:20:56 +0200

    Liste mails - Fonctionnel - OUF

--- exo_18.rb
+++ exo_18.rb
@@ -7,21 +7,20 @@ compteur = 00
 
  	while em <= 50
 
- 		if compteur < 9
- 			puts list << "jean.dupont.0"+"#{compteur}"+"@email.fr"
+ 		if compteur <= 9
+ 			list << "jean.dupont.0"+"#{compteur}"+"@email.fr"
  			compteur = compteur + 1
 
  		else
- 		puts list << "jean.dupont."+"#{compteur}"+"@email.fr"
- 		compteur = compteur + 1
+ 			list << "jean.dupont."+"#{compteur}"+"@email.fr"
+ 			compteur = compteur + 1
  			
     		
     	end
 
-	puts list
 	em = em + 1
 
-    end    
+    end  
 
-       
+    puts list
 	
