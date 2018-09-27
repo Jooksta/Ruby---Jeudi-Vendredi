3a406692d70ffa5fcf1f504ec5fd6be740fc5314
Jooksta <teslachipset@gmail.com>
2018-09-27 21:23:53 +0200

    Compter - Supression de commentaires pseudo-code que j'avais mis pour m'aider et que j'ai oublié d'enlever

--- exo_12.rb
+++ exo_12.rb
@@ -1,17 +1,13 @@
 puts "Salut, choisis un nombre pour moi ;)"
 print ">"
-number = gets.to_i #récupère le nombre donné par l'utilisateur
+number = gets.to_i 
 
-compteur = 0 #créer une variable intialisée à 0
-			 #on y ajoutera 1 jusqu'à avoir la meme valeur
-			 #que le nombre saisi par l'utilisateur
-
+compteur = 0 
+			
 while compteur <= number
-			 #tant que le nombre n'a pas la même valeur que
-			 #le nombre utilisateur, faire
-
-  puts compteur #imprime la valeur du compteur
-  compteur += 1 #la valeur du compteur augmente de 1
+			 
+  puts compteur 
+  compteur += 1 
 
 end
 
