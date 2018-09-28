28470f280a71d92a74be183bcfc6576d733f33b3
Jooksta <teslachipset@gmail.com>
2018-09-28 02:14:50 +0200

    Pyramide Illuminati - Modif phrase présentation

--- exo_20.rb
+++ exo_20.rb
@@ -1,9 +1,9 @@
-puts "Choisi un nombre entre 1 et 25"
+puts "Choisi un nombre entre 1 et 25 pour générer une belle pyramide ;)"
 puts ">"
 number=gets.to_i
 
 if number  < 1 || number > 25 
-	puts "Seulement entre 1 et 25"
+	puts "Seulement entre 1 et 25, désolé ^^"
 	
 else
 
